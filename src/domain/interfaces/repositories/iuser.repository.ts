0bb4a0a42dea283b2c8f913e5da9a7841d93daea
Mateus Zampol <m.zampol@consoftinformatica.it>
2022-11-12 16:19:35 +0100

    add method to interface

--- src/domain/interfaces/repositories/iuser.repository.ts
+++ src/domain/interfaces/repositories/iuser.repository.ts
@@ -1,5 +1,6 @@
 import { User } from "../../entities/user";
 
 export interface IUserRepository{
-    getUserByEmail(email: string) : Promise<User>
+    getUserByEmail(email: string) : Promise<User>,
+    createUser(user: User) : Promise<void>
 }
