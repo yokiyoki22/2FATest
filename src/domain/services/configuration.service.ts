827478cf95aa1a4b76b679033a5dd5b976399372
Mateus Zampol <m.zampol@consoftinformatica.it>
2022-11-12 18:19:37 +0100

    add connection string check at startup

--- src/domain/services/configuration.service.ts
+++ src/domain/services/configuration.service.ts
@@ -2,6 +2,7 @@ import dotenv from 'dotenv'
 
 dotenv.config();
 export const Port = process.env.PORT;
+export const ConnectionString = process.env.CONNECTION_STRING;
 
 export function CheckConfig(){
     // console.log(process.env)
@@ -9,6 +10,12 @@ export function CheckConfig(){
     if(Port === undefined){
         console.log("Invalid configuration.");
         console.log("Make sure to specify PORT in your environment variables.");
+        process.exit();
+    }
+
+    if(ConnectionString === undefined){
+        console.log("Invalid configuration.");
+        console.log("Make sure to specify CONNECTION_STRING in your environment variables.");
         process.exit();
     }
 }
