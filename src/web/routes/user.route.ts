81cb3662c58d3508edaa361847e48176ba2718f7
Mateus Zampol <m.zampol@consoftinformatica.it>
2022-11-12 17:44:50 +0100

    rename usersroute to userroute

--- src/web/routes/user.route.ts
+++ src/web/routes/user.route.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { UserController } from "../controllers/user.controller"
 
-export default function UsersRoute(
+export default function UserRoute(
     userController: UserController
 ){
     const router = Router();
