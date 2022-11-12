c77d8ebfc0f1746643459840659641c9761c0696
Mateus Zampol <m.zampol@consoftinformatica.it>
2022-11-12 16:21:20 +0100

    add user validator

---
+++ src/domain/validators/user.validator.ts
@@ -0,0 +1,45 @@
+import { injectable, inject } from "tsyringe";
+import { CreateUserCommand } from "../contracts/commands/create-user";
+import { ValidationResult } from "../contracts/validation/validation-result";
+import { IValidator } from "../interfaces/ivalidator";
+import { IUserRepository } from "../interfaces/repositories/iuser.repository";
+
+@injectable()
+export class UserValidator implements IValidator<CreateUserCommand>{
+    private _repo: IUserRepository;
+
+    constructor(@inject("IUsersRepository") usersRepo: IUserRepository){
+        this._repo = usersRepo;
+    }
+
+    async validate(item: CreateUserCommand): Promise<ValidationResult> {
+        const emailRegexp = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+
+        const errors = new Array<string>();
+
+        if(item.fullName === undefined){
+            errors.push('Missing fullName.');
+        }
+        if(item.email === undefined){
+            errors.push('Missing email.');
+        }
+        if(item.email && !emailRegexp.test(item.email)){
+            errors.push('Invalid email format.');
+        }
+        if(item.email && (await this._repo.getUserByEmail(item.email))){
+            errors.push('Email already in use.');
+        }
+        
+        if(errors.length == 0){
+            return {
+                isValid: true
+            };
+        }
+        else{
+            return {
+                isValid: false,
+                error: errors.reduce((prev, curr) => prev + " " + curr)
+            }
+        }
+    }
+}
